a08f65ba6d363a5e2e3124cf9dabde75de0c3b90
Hirdesh Garg <hirdeshgarg0012@gmail.com>
2020-08-26 18:55:13 +0530

    fragments added

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,14 +1,59 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
 
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import nl.joery.animatedbottombar.AnimatedBottomBar;
+
 public class MainActivity extends AppCompatActivity {
+
+    AnimatedBottomBar animatedBottomBar;
+    FragmentManager fragmentManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        animatedBottomBar = findViewById(R.id.animatedbottombar);
+        if(savedInstanceState == null){
+            fragmentManager = getSupportFragmentManager();
+            Home_Fragment home_fragment = new Home_Fragment();
+            fragmentManager.beginTransaction().replace(R.id.fragment_container,home_fragment)
+                    .commit();
+        }
+        animatedBottomBar.setOnTabSelectListener(new AnimatedBottomBar.OnTabSelectListener() {
+            @Override
+            public void onTabSelected(int lastIndex, @Nullable AnimatedBottomBar.Tab lastTab, int newIndex, @NotNull AnimatedBottomBar.Tab newTab) {
+                Fragment fragment = null;
+                switch (newTab.getId()){
+                    case R.id.tab_home:
+                        fragment = new Home_Fragment();
+                        break;
+                    case R.id.tab_alarm:
+                        fragment = new Profile_Fragment();
+                        break;
+                }
+                if(fragment!=null){
+                    fragmentManager = getSupportFragmentManager();
+                    fragmentManager.beginTransaction().replace(R.id.fragment_container,fragment)
+                            .commit();
+                }
+                else{
+
+                }
+            }
+
+            @Override
+            public void onTabReselected(int i, @NotNull AnimatedBottomBar.Tab tab) {
+
+            }
+        });
     }
 }
